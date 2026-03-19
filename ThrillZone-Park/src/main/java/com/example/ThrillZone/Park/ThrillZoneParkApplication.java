25d335fa1b0caa1800a918c53679e2168388961d
aayush <aayushrajpoot492@gmail.com>
2026-03-19 16:55:41 +0530

    Initial Spring Boot Setup - ThrillZone Park

---
+++ ThrillZone-Park/src/main/java/com/example/ThrillZone/Park/ThrillZoneParkApplication.java
@@ -0,0 +1,13 @@
+package com.example.ThrillZone.Park;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThrillZoneParkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThrillZoneParkApplication.class, args);
+	}
+
+}
---
+++ ThrillZone-Park/src/test/java/com/example/ThrillZone/Park/ThrillZoneParkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ThrillZone.Park;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThrillZoneParkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
