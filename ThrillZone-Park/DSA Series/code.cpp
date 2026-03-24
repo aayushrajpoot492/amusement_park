1afc0f47dd15f255ce6733dd0840422de7102fa3
ArhamAhmedAnsari786 <arhamahmadansari786@gmail.com>
2026-03-24 18:27:16 +0530

    hello

---
+++ ThrillZone-Park/DSA Series/code.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int age=251;
+    //char grade='a';
+    float fl=9.6;
+    //double 88.91;
+    bool ans=false;
+    
+    cout << "Arham\n""Ahmed\n";
+    cout << ans << endl;
+    cout << fl << endl;
+    return 0;
+}
