c159157467f98706c4acd3fec8974909595d56bd
ArhamAhmedAnsari786 <arhamahmadansari786@gmail.com>
2026-03-28 17:54:41 +0530

    commit

--- ThrillZone-Park/DSA Series/cin.cpp
+++
@@ -1,8 +0,0 @@
-#include<iostream>
-using namespace std;
-
-int main(){
-    int age;
-    cout << age << endl;
-    return 0;
-}
--- ThrillZone-Park/DSA Series/firstcode.cpp
+++
@@ -1,6 +0,0 @@
-#include<iostream>
-using namespace std;
-int main(){
-    cout << "Atches 23" << "Std"<< endl;
-    return 0;
-}
