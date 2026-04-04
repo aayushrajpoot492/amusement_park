4e7e838fc9c6ca9a181c65751ffad28fed752a0a
ArhamAhmedAnsari786 <arhamahmadansari786@gmail.com>
2026-04-04 18:50:45 +0530

    commit

--- ThrillZone-Park/src/main/resources/static/js/foodcourt.js
+++ ThrillZone-Park/src/main/resources/static/js/foodcourt.js
@@ -2,7 +2,7 @@
 
         masalaDosa: { name: "Masala Dosa", price: "₹120", calories: "320 kcal", protein: "8g", fat: "12g", desc: "Crispy rice crepe with potato mash.", img: "/images/Dinner/Masala dosa.jpeg" },
         idliSambar: { name: "Idli Sambar", price: "₹90", calories: "180 kcal", protein: "6g", fat: "2g", desc: "Steamed rice cakes with lentil stew.", img: "https://images.unsplash.com/photo-1589301760014-d929f3979dbc?w=400" },
-        hyderabadiBiryani: { name: "Hyderabadi Biryani", price: "₹350", calories: "550 kcal", protein: "22g", fat: "18g", desc: "Aromatic basmati rice cooked on dum.", img: "/images/FoodCourt/Biryaani.jpeg" },
+        hyderabadiBiryani: { name: "Hyderabadi Biryani", price: "₹350", calories: "550 kcal", protein: "22g", fat: "18g", desc: "Aromatic basmati rice cooked on dum.", img: "/images/Dinner/hyderabadi biryaani.jpeg" },
         appam: { name: "Appam", price: "₹150", calories: "210 kcal", protein: "4g", fat: "5g", desc: "Fermented rice pancakes.", img: "/images/FoodCourt/appam.jpeg" },
         meduVada: { name: "Medu Vada", price: "₹100", calories: "280 kcal", protein: "7g", fat: "15g", desc: "Deep fried lentil donuts.", img: "/images/FoodCourt/medu vada.jpeg" },
 
@@ -59,7 +59,12 @@
             <p style="color:#555; text-align:left; line-height:1.6;">${dish.desc}</p>
             <button id="orderBtn" class="order-btn" onclick="handleOrder()">Order Now</button>
         `;
-        document.getElementById('sidePage').style.width = "400px";
+      const drawer = document.getElementById('sidePage');
+          if (window.innerWidth <= 768) {
+              drawer.style.width = "75vw"; // Mobile par 75% width
+          } else {
+              drawer.style.width = "400px";
+          }
     }
     function handleOrder() {
     const btn = document.getElementById('orderBtn');
@@ -77,14 +82,14 @@
         btn.style.pointerEvents = "auto";
     }, 2000);
 }
-
+window.addEventListener('click', function(e) {
+    const sidePage = document.getElementById('sidePage');
+    if (sidePage.style.width !== "0px" && sidePage.style.width !== "0") {
+        if (!sidePage.contains(e.target) && !e.target.closest('.food-oval')) {
+            closeSidePage();
+        }
+    }
+});
     function closeSidePage() {
         document.getElementById('sidePage').style.width = "0";
     }
-    window.addEventListener('click', function(e) {
-  const sidePage = document.getElementById('sidePage');
-
-  if (!sidePage.contains(e.target) && !e.target.classList.contains('food-oval')) {
-    closeSidePage();
-  }
-});
