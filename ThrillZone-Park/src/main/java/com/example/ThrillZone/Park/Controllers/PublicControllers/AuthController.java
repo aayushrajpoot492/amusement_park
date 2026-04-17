47bec8ba330167f487f11a573575a1e77f72962d
aayush <aayushrajpoot492@gmail.com>
2026-04-17 17:47:43 +0530

    commit

--- ThrillZone-Park/src/main/java/com/example/ThrillZone/Park/Controllers/PublicControllers/AuthController.java
+++ ThrillZone-Park/src/main/java/com/example/ThrillZone/Park/Controllers/PublicControllers/AuthController.java
@@ -1,11 +1,19 @@
 package com.example.ThrillZone.Park.Controllers.PublicControllers;
 
+import com.example.ThrillZone.Park.Master.User_Master_Repo;
 import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AuthController {
+
+    @Autowired
+    private User_Master_Repo userRepo;
+
+    @Autowired
+    private org.springframework.security.crypto.password.PasswordEncoder passwordEncoder;
 
     @GetMapping("/signup-form")
     public String   Signup (){
--- ThrillZone-Park/src/main/java/com/example/ThrillZone/Park/Controllers/PublicControllers/InitialScreens.java
+++ ThrillZone-Park/src/main/java/com/example/ThrillZone/Park/Controllers/PublicControllers/InitialScreens.java
@@ -1,7 +1,6 @@
 package com.example.ThrillZone.Park.Controllers.PublicControllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -12,19 +11,4 @@ public class InitialScreens {
         return "splash-screen";
     }
 
-    @GetMapping("/home")
-    public String home () {
-        return "home";
-    }
-
-//   /* @GetMapping("/ParkProfile")
-//    public String parkprofile(){
-//        return "ParkProfile";
-//    }*/
-
-    @GetMapping("/ParkProfile")
-    public String events(Model model) {
-        model.addAttribute("activePage", "ParkProfile");
-        return "ParkProfile";
-    }
 }
